1cd432629b2596093860230cf79686d7a534f34a
JasonMadness <tobc_madness@mail.ru>
2024-01-31 07:31:03 +0300

    added two changes in scripts
    Added:
    - empty Player script
    - ClampPosition method to PlayerMovement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     private const string VERTICAL = "Vertical";
 
     [SerializeField] private float _speed;
+    [SerializeField] private Vector3 _boundaries;
 
     private void Move()
     {
@@ -17,9 +18,20 @@ public class PlayerMovement : MonoBehaviour
         Vector3 direction = new Vector3(horizontalDirection, 0.0f, verticalDirection);
         transform.position += direction * _speed * Time.deltaTime;
     }
+
+    private void ClampPosition()
+    {
+        Vector3 oldPosition = transform.position; 
+        float positionX = Math.Clamp(oldPosition.x, -_boundaries.x, _boundaries.x);
+        float positionY = oldPosition.y;
+        float positionZ = Math.Clamp(oldPosition.z, -_boundaries.z, _boundaries.z);
+        Vector3 newPosition = new Vector3(positionX, positionY, positionZ);
+        transform.position = newPosition;
+    }
     
     private void Update()
     {
         Move();
+        ClampPosition();
     }
 }
